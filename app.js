54029a45637e2dedd41498c1ec3ebb324dbe706d
Luis Perez <osorioperezluisfernando@gmail.com>
2024-10-10 10:19:46 -0500

    Consulta de registros de los usuarios segun el nit que se ingrese en el login

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({
 }));
 
 // Invocar la conexion a la base de datos
-require('./connectionBD/db');
+/* require('./connectionBD/db'); */
 
 //Integracion de plantillas ejs
 app.get('/', (req, res)=>{
@@ -38,7 +38,30 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/index', (req, res)=>{
-    res.render('index');
+    const nit = req.session.name;
+    if (!nit) {
+        return res.redirect('/');
+    }
+    connection.query('SELECT * FROM usuario where nit = ?', [nit], (error, results)=> {
+        if (error){
+            console.log(error);
+            return res.render('index', {
+                alert: true,
+                alertTitle: "Advertencia",
+                alertMessage: "No hay Registros de este usuario.",
+                alertIcon: "alert",
+                showConfirmButton: true,
+                timer: false,
+                ruta: "/"
+            });
+        }
+        function formatDate(dateString) {
+            const options = { day: '2-digit', month: 'numeric', year: 'numeric' };
+            const date = new Date(dateString);
+            return date.toLocaleDateString('es-ES', options);
+        }
+        res.render('index', {data: results, formatDate});
+    });
 });
 
 // Registrar usuarios
@@ -49,38 +72,78 @@ app.post('/register', async (req, res)=> {
 app.post('/auth', async (req, res)=> {
     const user = req.body.nit;
     const password = req.body.password;
-    let passwordhaash = await bcryptjs.hash(password, 8);
+    /* let passwordhaash = await bcryptjs.hash(password, 8); */
     if(user && password){
-        connection.query('SELECT * FROM usuario WHERE nit = ?', [user], async (error, results)=>{
-            if(results && results.length == 0 || !(await bcryptjs.compare(password, results[0].password))){
-                res.render('login', {
+        /* connection.query('SELECT * FROM usuario WHERE nit = ?', [user], async (error, results)=>{ */
+        connection.query('SELECT * FROM usuario WHERE nit = ?', [user], (error, results)=>{
+            if (error){
+                console.log(error);
+                return res.render('login', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Error en la base de datos",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: false,
+                    ruta: "/"
+                });
+            }
+            if/* (results.length === 0 || !(await bcryptjs.compare(password, results[0].password))){ */
+            (results.length === 0){
+                return res.render('login', {
                     alert:true,
                     alertTitle: "Error",
                     alertMessage: "Usuario y/o password invalidos.",
                     alertIcon: "error",
                     showConfirmButton: true,
                     timer: false,
-                    ruta: "login"
+                    ruta: "/"
                 });
-            }else{
-                req.session.name = results[0].nit
-                res.render('login', {
-                    alert:true,
-                    alertTitle: "Conexion Exitosa.",
-                    alertMessage: "¡LOGIN CORRECTO!",
-                    alertIcon: "success",
-                    showConfirmButton: false,
-                    timer: 1500,
-                    ruta: ''
-                });
-
-            }
-        })
+            }// Verificar si la contraseña es correcta
+               const dbPassword = results[0].password;
+               console.log("Contraseña en la base de datos:", dbPassword);
+               console.log("Contraseña introducida:", password);
+   
+               if (password !== dbPassword) {
+                   console.log("Contraseña incorrecta.");
+                   return res.render('login', {
+                       alert: true,
+                       alertTitle: "Error",
+                       alertMessage: "Usuario y/o contraseña inválidos.",
+                       alertIcon: "error",
+                       showConfirmButton: true,
+                       timer: false,
+                       ruta: "/"
+                   });
+               } 
+   
+               // Si todo es correcto, iniciar sesión
+               req.session.name = results[0].nit;
+               console.log("Usuario autenticado con éxito:", req.session.name);
+               res.render('login', {
+                   alert: true,
+                   alertTitle: "Conexión Exitosa",
+                   alertMessage: "¡LOGIN CORRECTO!",
+                   alertIcon: "success",
+                   showConfirmButton: false,
+                   timer: 1500,
+                   ruta: ('/index')
+               });
+           });
     }else{
-        res.send('Por favor ingrese un nit y una password')
+        res.render('login', {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Por favor, ingrese un NIT y una contraseña.",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: "/"
+        });
     }
 });
 
 app.listen(3000, (req, res)=> {
     console.log ('SERVER RUNNING IN HTTP://localhost:3000');
-})
+});
+
